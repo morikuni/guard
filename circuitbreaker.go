2021cec94e503b34331cc62d6387190de38024c7
morikuni <taihei.with+git@gmail.com>
2017-08-18 17:54:18 +0900

    Add CircuitBreaker

---
+++ circuitbreaker.go
@@ -0,0 +1,112 @@
+package guard
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"time"
+)
+
+func CircuitBreaker(window Window, threashold float64, backoff BackoffStrategy) Guard {
+	window.Reset()
+	cb := &circuitBreaker{
+		window,
+		threashold,
+		close,
+		backoff.Reset(),
+	}
+	return cb
+}
+
+const (
+	close int32 = iota
+	halfopen
+	open
+)
+
+var ErrCircuitBreakerOpen = errors.New("circuit breaker open")
+
+type circuitBreaker struct {
+	window     Window
+	threashold float64
+	state      int32
+	backoff    BackoffStrategy
+}
+
+func (cb *circuitBreaker) Call(ctx context.Context, f func(context.Context) error) error {
+	state, available := cb.currentState()
+	if !available {
+		return ErrCircuitBreakerOpen
+	}
+
+	err := f(ctx)
+	switch err {
+	case nil:
+		cb.succeed(state)
+	case context.Canceled:
+		// this is normal, so do nothing.
+	default:
+		// context.DeadlimeExceeded and other errors are regarded as failure.
+		cb.fail(state)
+	}
+	return err
+}
+
+func (cb *circuitBreaker) succeed(state int32) {
+	cb.window.PutSuccess()
+	switch state {
+	case close:
+	case halfopen:
+		if cb.change(state, close) {
+			cb.backoff = cb.backoff.Reset()
+		}
+	default:
+		panic("never come here")
+	}
+}
+
+func (cb *circuitBreaker) fail(state int32) {
+	cb.window.PutFailure()
+	switch state {
+	case close:
+		if cb.window.FailureRate() >= cb.threashold {
+			cb.open(state)
+		}
+	case halfopen:
+		cb.open(state)
+	default:
+		panic("never come here")
+	}
+}
+
+func (cb *circuitBreaker) currentState() (state int32, available bool) {
+	switch atomic.LoadInt32(&cb.state) {
+	case close:
+		return close, true
+	case halfopen:
+		return halfopen, true
+	case open:
+		return open, false
+	}
+	panic("never come here")
+}
+
+func (cb *circuitBreaker) change(from, to int32) bool {
+	return atomic.CompareAndSwapInt32(&cb.state, from, to)
+}
+
+func (cb *circuitBreaker) open(state int32) {
+	if cb.change(state, open) {
+		println("open")
+		time.AfterFunc(cb.backoff.NextInterval(), func() {
+			cb.change(open, halfopen)
+		})
+	}
+}
+
+func (cb *circuitBreaker) close(state int32) {
+	if cb.change(state, close) {
+		cb.backoff = cb.backoff.Reset()
+		cb.window.Reset()
+	}
+}
---
+++ window.go
@@ -0,0 +1,66 @@
+package guard
+
+import (
+	"sync"
+)
+
+type Window interface {
+	FailureRate() float64
+	PutSuccess()
+	PutFailure()
+	Reset()
+}
+
+type countBasedWindow struct {
+	idx            int
+	failures       int
+	size           int
+	failureHistory []bool
+	mu             sync.RWMutex
+}
+
+func CountWindow(size int) Window {
+	return &countBasedWindow{
+		size:           size,
+		failureHistory: make([]bool, size),
+	}
+}
+
+func (w *countBasedWindow) FailureRate() float64 {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return float64(w.failures) / float64(w.size)
+}
+
+func (w *countBasedWindow) PutSuccess() {
+	w.put(false)
+}
+
+func (w *countBasedWindow) PutFailure() {
+	w.put(true)
+}
+
+func (w *countBasedWindow) Reset() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	for i := range w.failureHistory {
+		w.failureHistory[i] = false
+	}
+	w.failures = 0
+	w.idx = 0
+}
+
+func (w *countBasedWindow) put(failure bool) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.failureHistory[w.idx] {
+		w.failures--
+	}
+	if failure {
+		w.failures++
+	}
+
+	w.failureHistory[w.idx] = failure
+	w.idx = (w.idx + 1) % w.size
+}
