1ffb19435efd5163c00efa7d3674f01f5e27b6f2
morikuni <taihei.with+git@gmail.com>
2017-08-25 15:57:25 +0900

    Support infinite retry

--- retry/retry.go
+++ retry/retry.go
@@ -7,27 +7,52 @@ import (
 	"github.com/morikuni/guard"
 )
 
+var Inf int = -1
+
 func New(n int, backoff guard.Backoff) guard.Guard {
 	return guard.GuardFunc(func(ctx context.Context, f func(context.Context) error) error {
 		bo := backoff.Reset()
+		stop := stopper(n)
 
-		var err error
-		for i := 0; i <= n; i++ {
-			if i > 0 {
-				t := time.NewTimer(bo.NextInterval())
-				defer t.Stop()
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case <-t.C:
-				}
-			}
-
-			err = f(ctx)
+		for {
+			err := f(ctx)
 			if err == nil {
 				return nil
 			}
+
+			if stop() {
+				return err
+			}
+
+			t := time.NewTimer(bo.NextInterval())
+			defer t.Stop()
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-t.C:
+			}
 		}
-		return err
 	})
+}
+
+func stopper(n int) func() bool {
+	if n < -1 {
+		return inf
+	}
+	return count(n)
+}
+
+func inf() bool {
+	return false
+}
+
+func count(n int) func() bool {
+	i := 0
+	return func() bool {
+		i++
+		if i > n {
+			return true
+		}
+		return false
+	}
 }
