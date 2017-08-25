c18629607e6a6babaa33cb9a4b616d24abad6d2a
morikuni <taihei.with+git@gmail.com>
2017-08-25 16:30:23 +0900

    Make Retry faster

--- retry/retry.go
+++ retry/retry.go
@@ -11,17 +11,16 @@ var Inf int = -1
 
 func New(n int, backoff guard.Backoff) guard.Guard {
 	return guard.GuardFunc(func(ctx context.Context, f func(context.Context) error) error {
-		bo := backoff.Reset()
-		stop := stopper(n)
+		if f(ctx) == nil {
+			return nil
+		}
 
-		for {
-			err := f(ctx)
+		bo := backoff.Reset()
+		var err error
+		for i := 0; i < n || n < 0; i++ {
+			err = f(ctx)
 			if err == nil {
 				return nil
-			}
-
-			if stop() {
-				return err
 			}
 
 			t := time.NewTimer(bo.NextInterval())
@@ -32,27 +31,6 @@ func New(n int, backoff guard.Backoff) guard.Guard {
 			case <-t.C:
 			}
 		}
+		return err
 	})
-}
-
-func stopper(n int) func() bool {
-	if n < -1 {
-		return inf
-	}
-	return count(n)
-}
-
-func inf() bool {
-	return false
-}
-
-func count(n int) func() bool {
-	i := 0
-	return func() bool {
-		i++
-		if i > n {
-			return true
-		}
-		return false
-	}
 }
